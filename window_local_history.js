f6700b81cee0b57f229ea2ef93c5fff7a5e4a929
sushilv3 <sushilv3rma@gmail.com>
2021-05-08 17:32:08 +0530

    added window, location and history functionality test file

---
+++ window_local_history.js
@@ -0,0 +1,18 @@
+// alert("hello");
+window.document.body.style.backgroundColor="tomato";
+window.document.title="kodecamp";
+window.history.forward();
+window.history.back();
+// window.location.reload();
+window.open();
+console.dir(window);
+console.log(window.innerHeight);
+
+console.log(location.hostname);
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.protocol);
+console.log(location.port);
+
+console.dir(history);
+console.dir();
